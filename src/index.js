f5e194d671c97d1e12ba057e0e9fbc207d18a07e
Sofi06ms <sofimendozasilva7@gmail.com>
2026-06-02 08:27:18 -0600

    configuracion para render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import app from "./app.js";
 import "./database.js";
-app.listen(3000,()=>
-console.log("Server running on port 3000"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

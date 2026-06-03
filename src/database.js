609d971c05a1746ba42d0b827b46ba842f7a5795
Sofi06ms <sofimendozasilva7@gmail.com>
2026-06-03 06:48:26 -0600

    Actualiza conexion MongoDB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb+srv://sofimendozasilva7_db_user:BzoOTTq8xcPzZRm2@cluster0.llgcsmp.mongodb.net/?appName=Cluster0")
+mongoose.connect("mongodb+srv://sofimendozasilva7_db_user:Sofiamen06@cluster0.llgcsmp.mongodb.net/?appName=Cluster0")
 .then((cnn)=> console.log ("Conected to MongoDB") )
 .catch((error) => console.error ("Error conecting to MongoDB", error) )
 
